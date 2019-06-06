4cc7245ae57a7d006f72c2db745adbb23122a018
Juneau <jungwon.limm@gmail.com>
2019-06-06 19:29:50 -0400

    Setting to get started

--- movie-browser/src/App.js
+++ movie-browser/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+const movie = {
+  title: 'title name',
+  description: 'movie description'
+}
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <p> { props.title } </p>
+      <p> { props.description } </p>
+    </div>
+  )
+}
+
 const App = () => {
   return (
     <div>
       Movie Browser
+      <Card {...movie}/>
     </div>
   );
 }
---
+++ movie-browser/src/Card.js
@@ -0,0 +1,8 @@
+// failed to import into App.js
+import React from 'react';
+
+const Card = () => {
+    return (
+        <p>test</p>
+    )
+}
