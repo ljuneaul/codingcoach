97f861c9abb1cfefd195d31affa209d7a70c1170
Juneau <jungwon.limm@gmail.com>
2019-06-10 07:52:45 -0400

    moved ApiReader call back to app.js

--- movie-browser/src/App.js
+++ movie-browser/src/App.js
@@ -1,15 +1,33 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { CardContainer } from './CardContainer'
+import CardContainer from './CardContainer'
+import ApiReader from './model/ApiReader';
 
-export const url = 'https://ghibliapi.herokuapp.com/films'
+// *** constant(s)
+const url = 'https://ghibliapi.herokuapp.com/films'
 
-const App = () => {
-  return (
-    <div>
-      <CardContainer />
-    </div>
-  )
+class App extends React.Component {
+  state = {movies: []};
+
+  // *** running ApiReader when mounted
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const data = await ApiReader(url);
+    this.setState({ movies: data });
+  }
+
+  // *** display
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <CardContainer movies={movies}  />
+      </div>
+    )
+  }
 }
 
 export default App
--- movie-browser/src/CardContainer.js
+++ movie-browser/src/CardContainer.js
@@ -1,28 +1,15 @@
-import React from 'react';
-import Card from './view/Card';
-import ApiReader from './model/ApiReader';
-import { url } from './App';
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Card from './view/Card'
 
-export class CardContainer extends React.Component {
-  state = {movies: []};
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  async fetchData() {
-    const data = await ApiReader(url);
-    this.setState({ movies: data });
-  }
-
-  render() {
-    const { movies } = this.state;
-    return (
-        <div className='row'>
-            {this.state.movies.map(movie => 
-                <Card key={movie.id} {...movie} />
-            )}
-        </div>
-    )
-  }
+const CardContainer = (props) => {
+  return (
+    <div className='row'>
+      {props.movies.map(movie =>
+        <Card key={movie.id} {...movie} />
+      )}
+    </div>
+  )
 }
+
+export default CardContainer
