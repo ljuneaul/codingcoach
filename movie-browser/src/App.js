34e676d588831b235b9cd193651db3a00fdd2987
Juneau <jungwon.limm@gmail.com>
2019-06-12 07:30:53 -0400

    made button

--- movie-browser/src/App.js
+++ movie-browser/src/App.js
@@ -8,9 +8,11 @@ import ApiReader from './model/ApiReader';
 const url = 'https://ghibliapi.herokuapp.com/films'
 
 // TODO: fix sorting by year. have no idea why it works for score but not for year
+// TODO: makes button working
+
 // *** Actual App
 class App extends React.Component {
-  state = {movies: [], featuredMovieIndex: 0 };
+  state = {movies: [], featuredMovieIndex: 0, buttonText: "View all" };
 
   // *** running ApiReader when mounted
   componentDidMount() {
@@ -20,6 +22,19 @@ class App extends React.Component {
   pickRandomMovieIndex = () => this.setState({
     featuredMovieIndex: Math.floor(Math.random() * this.state.movies.length)
   })
+
+  handleClickButton = () => {
+    if (this.state.buttonText === "View all") {
+      this.setState({
+        buttonText: "hide all"
+      })
+    } else {
+      this.setState({
+        buttonText: "View all"
+      })
+    }
+    
+  }
 
   async fetchData() {
     const data = await ApiReader(url)
@@ -32,11 +47,12 @@ class App extends React.Component {
 
   // *** display
   render() {
-    const { movies, featuredMovieIndex } = this.state;
+    const { movies, featuredMovieIndex, buttonText } = this.state;
     return (
       <div>
         <FeaturedMovie movie={movies[featuredMovieIndex]}  />
         <CardContainer movies={movies}  />  {/* limit number of data */}
+        <button onClick={this.handleClickButton}>{ buttonText }</button>
       </div>
     )
   }
--- movie-browser/src/view/FeaturedMovie.js
+++ movie-browser/src/view/FeaturedMovie.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import CardDetail from './CardDetail.js'
 
+// TODO: makes toggle for detail view work. have no idea why it's not working for this one
 // toggle: https://stackoverflow.com/questions/29913387/show-hide-components-in-reactjs
 class FeaturedMovie extends React.Component{
   state = {isDetailShown: false}
 
   handleClick = () => {
     this.setState(preState => ({isDetailShown: !preState.isDetailShown}));
-    console.log('clicked')
+    console.log(this.props.movie)
   }
 
   render() {
-    const { movie } = this.props;
-    const { title, iDonotExist = 'blala', director, release_date, isDetailShown } = movie
+    const { movie, isDetailShown } = this.props;
+    const { title, director, release_date } = movie
     return (
       <div className="card" onClick={() => this.handleClick()}>
           <div className="row card-body">
             <div className="col-sm-6">
-              <h5 className="card-title"> { title} - {iDonotExist } </h5>
+              <h5 className="card-title"> { title } </h5>
               <p className="card-text"> { director } ({ release_date }) </p>
-              { isDetailShown ? <CardDetail { ...(this.props) }/> : null}
+              { isDetailShown ? <CardDetail { ...(this.props.movie) }/> : null}
             </div>
-          <img className="col-sm-6" src="https://via.placeholder.com/200x100.png" alt="sans"/>
+          <img className="col-sm-6" src="https://via.placeholder.com/300x150.png" alt="sans"/>
         </div>
       </div>
     );
