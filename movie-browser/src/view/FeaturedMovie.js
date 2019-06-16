a9378abcdbbbc0c9cb3ee3536966e0b24a22d018
Juneau <jungwon.limm@gmail.com>
2019-06-15 23:00:51 -0400

    restyled FeaturedMovie

--- movie-browser/src/view/FeaturedMovie.js
+++ movie-browser/src/view/FeaturedMovie.js
@@ -19,12 +19,12 @@ class FeaturedMovie extends React.Component{
     return (
       <div className="card" onClick={this.handleClick}>
           <div className="row card-body">
+            <img className="col-sm-6" src="https://via.placeholder.com/300x150.png" alt={title}/>
             <div className="col-sm-6">
               <h5 className="card-title"> {title} </h5>
               <p className="card-text"> {director} ({release_date}) </p>
               {shouldShowDetails ? <CardDetail {...(this.props.movie)}/> : null}
             </div>
-          <img className="col-sm-6" src="https://via.placeholder.com/300x150.png" alt="sans"/>
         </div>
       </div>
     );
